f74fc2e6e4ea350f272149cf2609650cb6df7820
Nguyen Minh Phuong <phuongminhnguyen123a@gmail.com>
2023-07-05 17:09:41 +0700

    code

--- src/server.js
+++ src/server.js
@@ -7,16 +7,19 @@ const app = express(); //app express
 const port = process.env.PORT || 8888; //port => hardcode
 const hostname = process.env.HOST_NAME;
 
+// config static files
+app.use(express.static(path.join(__dirname, "public")));
+
 //config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//khai bao route
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
+// khai bao route
 app.get("/", (req, res) => {
+  res.send("Hello World! & nodemon");
+});
+
+app.get("/phuong", (req, res) => {
   // res.send("Hello World!");
   res.render("sample.ejs");
 });
